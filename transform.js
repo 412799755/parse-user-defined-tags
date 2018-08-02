5be16fc7c8bd6087d62668c1accbfe2c761cc943
luoy <luoy@schbrain.com>
2018-08-02 15:52:37 +0800

    解析出带自定义属性的文本

--- transform.js
+++ transform.js
@@ -1,14 +1,23 @@
 function transform(htmlStr){
     let reg = /(<red>|<\/red>)/g
     let testStack = new Stack
-    let tmp = {}
+    let tmp
+    let arr = new BulidArray(htmlStr)
+    let result = {}
     while(tmp){
+        // tmp用来保存每次匹配到的结果
         tmp = reg.exec(htmlStr)
        if(tmp && !testStack.test(tmp[0])) {
-            return testStack.iscorrect
+            result.iscorrect = testStack.iscorrect
+            return result
+       } else if(!!tmp){
+            console.log(tmp)
+            arr.build(tmp.index,tmp[0] =='<red>',tmp[0])
        }
     }
-    return !testStack.stack.length
+    result.value = arr.result
+    result.iscorrect = !testStack.stack.length
+    return result
 }
 //用来验证格式是否正确的栈
 function Stack(){
@@ -28,7 +37,15 @@ function Stack(){
 function BulidArray(str){
     this.result = []
     this.str = str
-    this.build = function(index){
+    this.lastIndex = 0
+    this.build = function(index,isStart,tag){
+            let obj = {}
+              obj.value = str.substring(this.lastIndex,index)
+            this.lastIndex = index + tag.length
+            if(!isStart){
+                obj.color = 'red'
+            }
+              this.result.push(obj)
 
     }
 }
